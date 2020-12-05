d2f3fbd706f1e58a42d9556ba841be5c2040f04c
Lgdev07 <luan_saopaulo06@hotmail.com>
2020-12-05 20:14:34 -0300

    create a function to refresh token when it is invalid

--- services/twitch/twitch.go
+++ services/twitch/twitch.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -14,6 +15,8 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
+
+var updatedBearerToken string
 
 type Twitch struct {
 	gorm.Model
@@ -26,8 +29,8 @@ func Initialize(wg *sync.WaitGroup, db *gorm.DB) {
 
 	go func() {
 		for {
-			time.Sleep(50 * time.Second)
 			MakeRequest(db)
+			time.Sleep(50 * time.Second)
 		}
 	}()
 
@@ -72,8 +75,11 @@ func Request(url string) bool {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	bearerToken := getBearerToken()
+
 	req.Header.Set("client-id", os.Getenv("CLIENT_ID"))
-	req.Header.Set("Authorization", os.Getenv("BEARER_TOKEN"))
+	req.Header.Set("Authorization", bearerToken)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -94,6 +100,29 @@ func Request(url string) bool {
 		log.Fatal(err)
 	}
 
+	if responseInterface["data"] == nil {
+		fmt.Println("Invalid Token, creating a new one...")
+
+		input, err := ioutil.ReadFile(".env")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		lines := strings.Split(string(input), " ")
+		token := lines[len(lines)-1]
+		newToken := createNewToken()
+		newOutput := strings.Replace(string(input), token, newToken, 1)
+
+		err = ioutil.WriteFile(".env", []byte(newOutput), 0644)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		updatedBearerToken = fmt.Sprint("Bearer ", newToken)
+
+		return Request(url)
+	}
+
 	dataResponse := responseInterface["data"].([]interface{})
 
 	if len(dataResponse) > 0 {
@@ -101,6 +130,52 @@ func Request(url string) bool {
 	}
 
 	return false
+
+}
+
+func getBearerToken() string {
+	token := os.Getenv("BEARER_TOKEN")
+	if updatedBearerToken == "" {
+		return token
+	}
+
+	if token == updatedBearerToken {
+		return token
+	}
+
+	return updatedBearerToken
+}
+
+func createNewToken() string {
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	url := fmt.Sprintf("https://id.twitch.tv/oauth2/token?client_id=%v&client_secret=%v&grant_type=client_credentials", clientID, clientSecret)
+
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseInterface := make(map[string]interface{})
+
+	err = json.Unmarshal(body, &responseInterface)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return responseInterface["access_token"].(string)
 
 }
 
