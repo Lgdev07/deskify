0374c0e176273c115fe283afa4e46f9e4638ddba
Luan Gomes <luan_saopaulo06@hotmail.com>
2022-02-13 15:36:27 -0300

    update gorm and add function documentation

--- services/tasks/tasks.go
+++ services/tasks/tasks.go
@@ -6,9 +6,10 @@ import (
 	"time"
 
 	"github.com/gen2brain/beeep"
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
+// Task is the struct associated with database model.
 type Task struct {
 	gorm.Model
 	Name                string `gorm:"size:100;not null" json:"name"`
@@ -16,6 +17,7 @@ type Task struct {
 	IsDone              bool   `gorm:"default:false" json:"is_done"`
 }
 
+// Initialize runs the functionality.
 func Initialize(wg *sync.WaitGroup, db *gorm.DB) {
 	tasks, err := getValuesFromDatabase(db)
 	if err != nil {
@@ -24,7 +26,7 @@ func Initialize(wg *sync.WaitGroup, db *gorm.DB) {
 
 	for _, task := range *tasks {
 		wg.Add(1)
-		go RunFunction(task.Name, task.BeRememberedMinutes)
+		go runFunction(task.Name, task.BeRememberedMinutes)
 	}
 
 }
@@ -40,7 +42,7 @@ func getValuesFromDatabase(db *gorm.DB) (*[]Task, error) {
 	return tasks, nil
 }
 
-func RunFunction(name string, timer int) {
+func runFunction(name string, timer int) {
 	for {
 		time.Sleep(time.Duration(timer) * time.Minute)
 		err := beeep.Notify(name, "Remember to do Your Task!", "assets/task.png")
